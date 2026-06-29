5370b8716c0a2557af97db79b622bdaa35ececb9
Victor <vanuragmishra@gmail.com>
2026-06-29 17:34:00 +0530

    my third commit

--- testing.py
+++ testing.py
@@ -1,2 +1,62 @@
-print("My name is lakhan")
-print ("My name is shyamu")
+  Id CommandLine                                                                                                         
+  -- -----------                                                                                                         
+   1 try { . "c:\Users\Acer\AppData\Local\Programs\Microsoft VS Code\fcf604774b\resources\app\out\vs\workbench\contrib...
+   2 git -v                                                                                                              
+   3 git init                                                                                                            
+   4 pwd                                                                                                                 
+   5 print "myname"                                                                                                      
+   6 echo "myname"                                                                                                       
+   7 git status                                                                                                          
+   8 git config --list                                                                                                   
+   9 git remote -v                                                                                                       
+  10 ls -a                                                                                                               
+  11 ls                                                                                                                  
+  12 ls -a                                                                                                               
+  13 pwd                                                                                                                 
+  14 git init                                                                                                            
+  15 ls                                                                                                                  
+  16 ls -a                                                                                                               
+  17 ll                                                                                                                  
+  18 ls a                                                                                                                
+  19 ls -a                                                                                                               
+  20 clear                                                                                                               
+  21 nano myfile                                                                                                         
+  22 git nano myfile                                                                                                     
+  23 git touch myfile                                                                                                    
+  24 get -help                                                                                                           
+  25 get --help                                                                                                          
+  26 git --help                                                                                                          
+  27 git status                                                                                                          
+  28 git status                                                                                                          
+  29 git add .\testing.py                                                                                                
+  30 git add testing.py                                                                                                  
+  31 git status                                                                                                          
+  32 ls                                                                                                                  
+  33 git rm --cached .\testing.py                                                                                        
+  34 ls                                                                                                                  
+  35 git status                                                                                                          
+  36 git add testing.py                                                                                                  
+  37 ls                                                                                                                  
+  38 git status                                                                                                          
+  39 git commit -m "my first commit"                                                                                     
+  40 git status                                                                                                          
+  41 git status                                                                                                          
+  42 git restore .\testing.py                                                                                            
+  43 git status                                                                                                          
+  44 git add .\testing.py                                                                                                
+  45 git status                                                                                                          
+  46 git status                                                                                                          
+  47 git status                                                                                                          
+  48 git add remote origin https://github.com/vanuragmishra/shubham.git                                                  
+  49 git add remote origin "https://github.com/vanuragmishra/shubham.git"                                                
+  50 git add remote origin https://github.com/vanuragmishra/shubham.git                                                  
+  51 git add remote origin master https://github.com/vanuragmishra/shubham.git                                           
+  52 git remote add origin master https://github.com/vanuragmishra/shubham.git                                           
+  53 git remote add origin  https://github.com/vanuragmishra/shubham.git                                                 
+  54 git remote -v                                                                                                       
+  55 git push -u                                                                                                         
+  56 git push -u https://github.com/vanuragmishra/shubham.git                                                            
+  57 git status                                                                                                          
+  58 git commit -m "my second commit"                                                                                    
+  59 git status                                                                                                          
+  60 git push -u https://github.com/vanuragmishra/shubham.git  
